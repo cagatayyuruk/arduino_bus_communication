cbe0a6d8d2c215ec23251e12b9f4d67cf5d42658
Çağatay Yürük <cagatayyuruk@gmail.com>
2017-10-14 20:22:38 +0300

    Add files via upload
    master uno için. mega da gelen veriyi okuma eklenecek

---
+++ master/master.ino
@@ -0,0 +1,50 @@
+/*
+  Analog input, analog output, serial output
+
+ Reads an analog input pin, maps the result to a range from 0 to 255
+ and uses the result to set the pulsewidth modulation (PWM) of an output pin.
+ Also prints the results to the serial monitor.
+
+ The circuit:
+ * potentiometer connected to analog pin 0.
+   Center pin of the potentiometer goes to the analog pin.
+   side pins of the potentiometer go to +5V and ground
+ * LED connected from digital pin 9 to ground
+
+ created 29 Dec. 2008
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+
+
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  //[node_id][function_id][data][parity]
+  //[function_id] 0 read bits - 2 write bits 
+  //node 1 led yak
+  byte Bytes[]= {1,2,13,1};
+  Serial.write(Bytes,4);
+  delay(1000);
+  //node 1 led söndür
+  Bytes[3]= 0 ;
+  Serial.write(Bytes,4);
+  delay(1000);
+  //node 2 led yak
+  Bytes[0]=2;
+  Bytes[3]=1;  
+  Serial.write(Bytes,4);
+  delay(1000);
+  //node 2 led söndür
+  Bytes[3] = 0;
+  Serial.write(Bytes,4);
+  delay(1000);  
+}
---
+++ slave_uno/slave_uno.ino
@@ -0,0 +1,50 @@
+int incomingByte = 0;   // for incoming serial da
+byte Bytes[4] ;
+
+
+void setup() {
+        Serial.begin(9600);// opens serial port, sets data rate to 9600 bps
+        pinMode(13, OUTPUT);
+}
+
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+
+                Serial.readBytes(Bytes,4);
+                int pin = Bytes[2];  
+                int val;             
+
+                // say what you got:
+                //Serial.print("I received: ");
+                //Serial.print(Bytes[0]);
+                //Serial.print(Bytes[1]);
+                //Serial.print(Bytes[2]);
+                //Serial.println(Bytes[3]);
+                if (Bytes[0] == 2){
+                  switch(Bytes[1]){
+                    case 0: //read bit
+                    val = digitalRead(pin);
+                    if(val == HIGH){
+                      Bytes[3] = 1;
+                      }
+                    if(val == LOW){
+                      Bytes[3] = 0;
+                      }
+                    Serial.write(Bytes,4);
+                    break;
+                    case 2:
+                    val = Bytes[3]; 
+                    if(val == 0){ // 
+                      digitalWrite(pin, LOW);                      
+                      } 
+                    else if(val == 1){ // write bits
+                      digitalWrite(pin, HIGH);                      
+                      } 
+                    break;
+                    }
+                  }
+        }
+}
